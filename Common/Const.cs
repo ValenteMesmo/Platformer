9f4064e059a63cb33c4e9e3af3f81bebd3433469
valentemesmo <valentemesmo@gmail.com>
2020-12-16 23:32:17 -0300

    wip

--- Common/Const.cs
+++ Common/Const.cs
@@ -7,7 +7,7 @@
         public const int GRAVITY_ACCELERATION = 90;
         public const int GRAVITY_MAXSPEED = 3000;
 
-        public const int stoppingGravity = GRAVITY_MAXSPEED / 6;
+        public const int stoppingGravity = GRAVITY_MAXSPEED / 9;
         public const int jumpForce = GRAVITY_ACCELERATION * 30;
         public const int Grounded_Timer = 9;
 
